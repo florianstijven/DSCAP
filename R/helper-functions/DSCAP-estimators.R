bfcf212984752b26a53279dc16767abd4684c45b
florianstijven <florian.stijven@gmail.com>
2026-02-25 21:40:22 +0100

    implement bootstrap

--- R/helper-functions/DSCAP-estimators.R
+++ R/helper-functions/DSCAP-estimators.R
@@ -80,20 +80,21 @@ estimate_DSCAP <- function(data,
 # Function that computes the trial-level treatment effect estimates and
 # association measures, and performs the bootstrap for inferences.
 inference_DSCAP <- function(data,
-                        type,
-                        formula_Y,
-                        formula_S,
-                        formula_T,
-                        formula_CC = NULL,
-                        trial_var = "trial",
-                        treatment_var,
-                        target_trial,
-                        estimate_weights = FALSE,
-                        CC_weight_var,
-                        n_bootstrap) {
-  # Estimate the trial-level treatment effects and association measures in the
+                            type,
+                            formula_Y,
+                            formula_S,
+                            formula_T,
+                            formula_CC = NULL,
+                            trial_var = "trial",
+                            treatment_var,
+                            target_trial,
+                            estimate_weights = FALSE,
+                            CC_weight_var,
+                            B,
+                            alpha = 0.05) {
+  # Estimate the trial-level treatment effects and association measures for the
   # original data.
-  association_measures_df = estimate_DSCAP(
+  estimates_df <- estimate_DSCAP(
     data = data,
     type = type,
     formula_Y = formula_Y,
@@ -105,29 +106,48 @@ inference_DSCAP <- function(data,
     target_trial = target_trial,
     estimate_weights = estimate_weights,
     CC_weight_var = CC_weight_var
-  )
+  ) %>%
+    do.call(what = estimates_table_join)
+
+  # Function that takes in data and indices, and return the estimates.
+  boot_fun <- function(data, indices) {
+    data_resampled <- data[indices, ] # allows boot to select sample
+    estimates_list = estimate_DSCAP(
+      data = data_resampled,
+      type = type,
+      formula_Y = formula_Y,
+      formula_S = formula_S,
+      formula_T = formula_T,
+      formula_CC = formula_CC,
+      trial_var = trial_var,
+      treatment_var = treatment_var,
+      target_trial = target_trial,
+      estimate_weights = estimate_weights,
+      CC_weight_var = CC_weight_var
+    )
+    estimates_table_join(estimates_list[[1]], estimates_list[[2]]) %>%
+      pull(estimate) %>%
+      return()
+  }
   
   # Perform the bootstrap to obtain standard errors and confidence intervals for
   # the association measures.
-  bootstrap_replicates_df = bootstrap_DSCAP(
-    data = data,
-    type = type,
-    formula_Y = formula_Y,
-    formula_S = formula_S,
-    formula_T = formula_T,
-    formula_CC = formula_CC,
-    trial_var = trial_var,
-    treatment_var = treatment_var,
-    target_trial = target_trial,
-    estimate_weights = estimate_weights,
-    CC_weight_var = CC_weight_var,
-    n_bootstrap = n_bootstrap
-  )
+  boot_out <- boot::boot(data = df, statistic = boot_fun, R = B)
+  # Compute bootstrap CIs and SEs for all estimates in estimates_df.
+  estimates_df = estimates_df %>%
+    mutate(
+      CI_lower_bs = purrr::map_dbl(
+        1:nrow(estimates_df),
+        ~ boot::boot.ci(boot_out, type = "perc", index = .x, conf = 1 - alpha)$percent[4]
+      ),
+      CI_upper_bs = purrr::map_dbl(
+        1:nrow(estimates_df),
+        ~ boot::boot.ci(boot_out, type = "perc", index = .x, conf = 1 - alpha)$percent[5]
+      ),
+      SE_bs = apply(boot_out$t, 2, sd)
+    )
   
-  return(list(
-    association_measures_df = association_measures_df, 
-    bootstrap_replicates_df = bootstrap_replicates_df
-  ))
+  return(estimates_df)
 }
 
 # Function to compute association measures (i.e., the DSCAPs) given the table of
