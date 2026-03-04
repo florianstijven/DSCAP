637b3c1d9fac718c2fefa50c0adc514316633ad6
florianstijven <florian.stijven@gmail.com>
2026-03-04 14:12:59 +0100

    fix remaining issues

--- R/helper-functions/treatment-effect-estimators.R
+++ R/helper-functions/treatment-effect-estimators.R
@@ -486,7 +486,7 @@ compute_ipw_weights <- function(df, formula_T, target_trial) {
   # probability is 0.5 for control in all trials, and 0.5 for the active
   # treatment in the trial in which the active treatment is given and 0 for
   # other treatments.
-  browser()
+
   # For each trial, we need the treatment level corresponding to that trial's
   # active treatment. 
   treatment_int_by_trial_df = df %>%
